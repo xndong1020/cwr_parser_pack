f0c62f6f146ece30f8ddaae42336b22486046f42
Jeremy Gu <jgu@apra.com.au>
2022-03-30 23:59:39 +1100

    try fix

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     name="cwr_parser_pack",
     version="0.1",
     description="CWR file parser",
-    url="git@github.com:xndong1020/cwr_parser_pack.git",
+    url="https@github.com:xndong1020/cwr_parser_pack.git",
     author="JGU",
     author_email="jgu@apra.com.au",
     license="MIT",
-    packages=setuptools.find_namespace_packages(include=["cwr_parser_pack.*"]),
-    namespace_packages=["cwr_parser_pack"],
+    packages=setuptools.find_namespace_packages(include=["cwr_parser.*"]),
+    namespace_packages=["cwr_parser"],
     # zip_safe=False
 )
