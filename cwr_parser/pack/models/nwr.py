a3cdab8bf2613461eb00537a3865032010b13dfe
Jeremy Gu <jgu@apra.com.au>
2022-03-31 20:41:20 +1100

    try fix

--- cwr_parser/pack/models/nwr.py
+++ cwr_parser/pack/models/nwr.py
@@ -39,5 +39,4 @@ class Nwr(EntityBase):
         return getattr(self, key)
 
     def __setitem__(self, key: str, new_value):
-        self[key] = new_value
-        return
+        setattr(self, key, new_value)
